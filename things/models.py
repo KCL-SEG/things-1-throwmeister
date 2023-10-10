1280d04a963c9583d18d01306dc0284231ae16c9
AlexWickman <alexander.wickman@kcl.ac.uk>
2023-10-10 19:08:03 +0100

    Add restrictions to Thing model fields

--- things/models.py
+++ things/models.py
@@ -1,12 +1,13 @@
 from typing import Any
 from django.contrib.auth import models
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class BaseModel(models.Model):
     class Meta:
         abstract=True
 
 class Thing(BaseModel):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],unique=False ,blank=False)
--- things/tests.py
+++ things/tests.py
@@ -24,4 +24,20 @@ class ThingModelTestCase(TestCase):
     def test_testing(self):
         self._assert_thing_is_valid()
     
+    def test_quantity_min(self):
+        self.thing.quantity = -1
+        self._assert_thing_is_invalid()
+    
+    def test_quantity_just_min(self):
+        self.thing.quantity = 0
+        self._assert_thing_is_valid()
+    
+    def test_quantity_max(self):
+        self.thing.quantity = 101
+        self._assert_thing_is_invalid()
+    
+    def test_quantity_just_max(self):
+        self.thing.quantity = 100
+        self._assert_thing_is_valid()
+
     
