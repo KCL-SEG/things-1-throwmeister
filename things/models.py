212545a24f23701a7815d20bfc0f1dee4f1ece2d
AlexWickman <alexander.wickman@kcl.ac.uk>
2023-10-10 18:06:38 +0100

    Add Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import Model
 
 # Create your models here.
+
+class Thing(Model):
+    name = models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
+    
